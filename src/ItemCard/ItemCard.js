316e72970c6916eae0d79b11a67d54c775617327
Tyler Bowman <tbowman20@gmail.com>
2023-12-21 13:22:09 -0500

    first review

--- src/ItemCard/ItemCard.js
+++ src/ItemCard/ItemCard.js
@@ -1,22 +1,3 @@
-// import "./ItemCard.css";
-
-// const ItemCard = ({ item, onSelectedCard }) => {
-//   return (
-//     <div>
-//       <div>
-//         <img
-//           src={item.link}
-//           className="card__image"
-//           onClick={() => onSelectedCard(item)}
-//         />
-//       </div>
-//       <div className="card__name">{item.name}</div>
-//     </div>
-//   );
-// };
-
-// export default ItemCard;
-
 import "./ItemCard.css";
 
 const ItemCard = ({ item, onSelectedCard }) => {
